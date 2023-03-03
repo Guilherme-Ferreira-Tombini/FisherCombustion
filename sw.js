add0253fec4eae2b0933a2fdc9b4921dbe188924
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-03-03 15:26:04 -0300

    cache ajustado

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 let cacheName = "fisher-combustion";
-let filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
+let filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js", "/img/header.png", "/img/main.png",
+ "/img/imagem1.png", "/img/imagem2.png", "/img/seta_abaixo.png", "/img/pwa-icon-256.png", 
+ "/img/pwa-icon-512.png", "/img/pwa-iicon-256.png", "/img/pwa-iicon-512.png", "/favicon.ico"];
 
 self.addEventListener("install", (e) =>{
     e.waitUntil(
